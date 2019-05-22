736dd3069111050f979bfda227866a16a8cc41e4
andrazpovse <andraz.povse1@gmail.com>
2019-05-22 09:59:16 +0200

    TaintRank based on node value, link weight. Displaying top 100 results and their taintrank score.

--- develop/py.py
+++ develop/py.py
@@ -10,18 +10,36 @@ def print_G_info(G):
 def trace_taint(G, root):
     S = []
     S.append(root)
+    # set of visited nodes
     visited = set()
     visited.add(root)
+    G.node[root]['taint_rank'] = 1
     while S:
         # Pop a vertex from stack to visit next
         n = S.pop()
-        # Push all the neighbours of v in stack that are not visited   
+        # Get all in-edges of the node and
+        # calculate the funds of 'n' based on in-edges weights
+        n_funds = 0
+        # link is a tuple (predecessor, n)
+        for link in G.in_edges(n):
+            n_funds += G.get_edge_data(link[0], link[1])['weight']
+        # Push all the neighbours of n that are not visited in stack
         for neighbor in G[n].keys():
             if neighbor not in visited:
-                # TODO: Add some taint attribute based on in-links, weights, ...
+                # TODO: find out addresses of exchanges and dont spread taintrank through them
+                # TODO: find out addresses of mixers and DO spread taintrank throguh them
+                # TODO: test different approaches for spreading taint (e.g. BFS and distance from root, ...)
+                # Currently based on amount of tainted funds received -> gets smaller taintrank
+                # Check if entry exists and add to it, else initialize it with this taintrank
+                if 'taint_rank' in G.node[neighbor]:
+                    G.node[neighbor]['taint_rank'] += G.node[n]['taint_rank'] * (G.get_edge_data(n, neighbor)['weight']/n_funds)
+                else:
+                    G.node[neighbor]['taint_rank'] = G.node[n]['taint_rank'] * (G.get_edge_data(n, neighbor)['weight']/n_funds)
+
                 S.append(neighbor)
                 visited.add(neighbor)
-                global_tainted_addresses.append(neighbor)
+    return visited
+
 
 # Thief = 1MAazCWMydsQB5ynYXqSGQDjNQMN3HFmEu
 G = nx.read_edgelist("../data/bitcoin.edgelist",create_using=nx.DiGraph())
@@ -30,6 +48,9 @@ print(nx.is_directed(G))
 print_G_info(G)
 
 # Trace taintedness starting from root, going to all neighbors
-global_tainted_addresses = [thief_addr]
-trace_taint(G, thief_addr)
-print("Number of tainted addresses: ", len(global_taint))
+tainted_addresses = trace_taint(G, thief_addr)
+print("Number of tainted addresses: ", len(tainted_addresses))
+# Dictionary of taint scores of each node
+taint_scores = nx.get_node_attributes(G,'taint_rank')
+sorted_taint_cores = sorted(taint_scores.items(), key=lambda kv: kv[1], reverse=True)
+print(sorted_taint_cores[:100])
